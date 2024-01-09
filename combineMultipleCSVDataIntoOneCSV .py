7e5b42eb9d46b6cd265d747acf5f591f97a305ab
bokele wakiza franck <bwakiza@gmail.com>
2024-01-09 15:36:35 +0200

    Python: Combine Multiple CSV Data into One CSV

---
+++ combineMultipleCSVDataIntoOneCSV .py
@@ -0,0 +1,28 @@
+import csv
+
+new_css_data = []
+for year in range(2021, 2024):
+    filename = f'salaries-{year}.csv'
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            new_css_data.append([
+                year,
+                row['years_of_experience'] if 'years' not in row else row['years'],
+                row['salary']
+            ])
+
+with open('salaries-full.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow([
+        'survey_year',
+        'years_of_experience',
+        'salary'
+    ])
+
+    writer.writerows(new_css_data)
+
+
+
+
+
